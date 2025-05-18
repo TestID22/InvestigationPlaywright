db47ed6fb737585d79aa0696e93118f6a247b6ec
VSabadash <vitaliiseptember@gmail.com>
2025-05-18 16:59:55 +0300

    added a comment if fails

--- tests/test_google.py
+++ tests/test_google.py
@@ -2,4 +2,4 @@
 def test_google_title(browser):
     browser.goto("https://www.google.com")
     print(browser.title())
-    assert "Google" in browser.title()
+    assert "Google" in browser.title(), "Title does not contain 'Google'"
