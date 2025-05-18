2cdf2bd47968ce80b14fb4cdc368b07ed0a32c1d
VSabadash <vitaliiseptember@gmail.com>
2025-05-18 16:52:35 +0300

    deleted

--- first_tests.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    print(type(browser))
-    page = browser.new_page()
-    page.goto("https://www.google.com")
-    print(page.title())
-    browser.close()
--- framework/browser.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from playwright.sync_api import sync_playwright
-
-@pytest.fixture
-def launch_browser(headless=False):
-    p = sync_playwright().start()
-    browser = p.chromium.launch(headless=headless)
-    return p, browser
-
