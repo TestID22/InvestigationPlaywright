b869f9b6f8701a9e4b591a6942f04e02c99f6884
VSabadash <vitaliiseptember@gmail.com>
2025-05-31 09:02:12 +0300

    added the arg to open browser in full size

--- framework/web_browser.py
+++ framework/web_browser.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import sync_playwright
 
+
 # This function initializes a Playwright browser instance.
 def web_browser():
     p = sync_playwright().start()
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=False, args=['--start-maximized'],)
     page = browser.new_page()
     return page, browser
 
